1cb1a0de1a37aa5dffae46c86bd600dc5aaf1e17
UserOfNames <lightninguy.yoyo@gmail.com>
2025-10-16 17:31:21 -0400

    squash! incomplete

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -11,23 +11,22 @@ use scanner::Scanner;
 pub fn compile(source: &str) -> InterpretResult<Chunk> {
     let c = Chunk::new();
 
-    let mut parser = Parser::new();
-
     let scanner = Scanner::new(source);
+    let mut parser = Parser::new(scanner);
 
-    for token_res in scanner {
-        let token = match token_res {
-            Ok(t) => t,
-            Err(e) => {
-                parser.erred = true;
-                eprintln!("[Line {}] Syntax error: {}\n", e.line(), e);
-                parser.panicking = true;
-                continue;
-            }
-        };
-        parser.panicking = false;
-    }
-
+    // for token_res in scanner {
+    //     let token = match token_res {
+    //         Ok(t) => t,
+    //         Err(e) => {
+    //             parser.erred = true;
+    //             eprintln!("[Line {}] Syntax error: {}\n", e.line(), e);
+    //             parser.panicking = true;
+    //             continue;
+    //         }
+    //     };
+    //     parser.panicking = false;
+    // }
+    //
     if parser.erred {
         Err(InterpretError::Compiler)
     } else {
--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -1,16 +1,43 @@
-use super::scanner::Scanner;
+use super::scanner::{Scanner, ScannerError};
+use super::token::Token;
 
 #[derive(Debug)]
-pub struct Parser {
+pub struct Parser<'a> {
+    pub scanner: Scanner<'a>,
+    pub current: Token<'a>,
+    pub previous: Token<'a>,
     pub erred: bool,
     pub panicking: bool,
 }
 
-impl Parser {
-    pub fn new() -> Self {
+impl<'a> Parser<'a> {
+    pub fn new(mut scanner: Scanner<'a>) -> Self {
+        // TODO: Proper error model
+        let previous = scanner.next().unwrap().unwrap();
+        let current = scanner.next().unwrap().unwrap();
         Self {
+            scanner,
+            current,
+            previous,
             erred: false,
             panicking: false,
         }
+    }
+
+    fn advance(&mut self) -> Option<Token<'a>> {
+        while let Some(token_res) = self.scanner.next() {
+            match token_res {
+                Ok(t) => return Some(t),
+                Err(e) => self.report_err(&e),
+            }
+        }
+
+        None
+    }
+
+    fn report_err(&mut self, error: &ScannerError) {
+        self.erred = true;
+        self.panicking = true;
+        eprintln!("[Line {}] Syntax error: {}\n", error.line(), error);
     }
 }
